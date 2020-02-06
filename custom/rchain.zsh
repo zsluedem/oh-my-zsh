09fbf25f9ff14952b92ee87a71fc98a07cdfe941
Will Qiu <zsluedem06@gmail.com>
2020-02-06 13:42:38 +0800

    update rchain command

--- custom/rchain.zsh
+++ custom/rchain.zsh
@@ -1,4 +1,5 @@
 alias rchainclean='sbt clean rholang/bnfc:clean rholang/bnfc:generate'
-alias rchaincompile='sbt compile'
+alias rchaincompile='_JAVA_OPTIONS="-Xms2G -Xmx4G -Xss2m -XX:MaxMetaspaceSize=1G -Dsbt.task.timings=true -Dsbt.task.timings.on.shutdown=true -Dsbt.task.timings.threshold=2000" sbt compile'
 alias rchaindocker='sbt node/docker:publishLocal'
-alias rchainrecompile='JAVA_OPTIONS="-Xms2G -Xmx4G -Xss2m -XX:MaxMetaspaceSize=1G -Dsbt.task.timings=true -Dsbt.task.timings.on.shutdown=true -Dsbt.task.timings.threshold=2000" sbt clean rholang/bnfc:clean rholang/bnfc:generate compile node/docker:publishLocal'
+alias rchainrecompile='_JAVA_OPTIONS="-Xms2G -Xmx4G -Xss2m -XX:MaxMetaspaceSize=1G -Dsbt.task.timings=true -Dsbt.task.timings.on.shutdown=true -Dsbt.task.timings.threshold=2000" sbt clean rholang/bnfc:clean rholang/bnfc:generate compile node/docker:publishLocal'
+alias buildrchaintestimage='docker build -t rchain-integration-tests:latest .'
